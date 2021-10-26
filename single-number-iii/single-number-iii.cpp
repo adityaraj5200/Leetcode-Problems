65dad2b2cc6c13cb0a347b459fb792d59222116f
Aditya Raj <72252036+adityaraj5200@users.noreply.github.com>
2021-10-27 01:09:08 +0530

    Time: 4 ms (98.50%), Space: 9.8 MB (93.13%) - LeetHub

---
+++ single-number-iii/single-number-iii.cpp
@@ -0,0 +1,18 @@
+class Solution{
+public:
+    vector<int> singleNumber(vector<int>& nums){
+        int bitxor = 0,a = 0,b = 0,shifts = 0;
+        for(int &val: nums)
+            bitxor ^= val;
+        while((bitxor&1) == 0){
+            shifts++;
+            bitxor >>= 1;
+        }
+        bitxor = 1<<shifts;
+        for(int &val: nums)
+            if(val & bitxor)
+                a ^= val;
+            else b ^= val;
+        return {a,b};
+    }
+};
