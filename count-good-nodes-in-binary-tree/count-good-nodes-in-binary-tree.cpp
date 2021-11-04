e88656c774f470b2b1852779d55a4272d0c0d1ab
Aditya Raj <72252036+adityaraj5200@users.noreply.github.com>
2021-11-05 04:04:41 +0530

    Time: 124 ms (64.18%), Space: 86.4 MB (10.78%) - LeetHub

---
+++ count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.cpp
@@ -0,0 +1,55 @@
+void __print(int x) {cout << x;}
+void __print(long x) {cout << x;}
+void __print(long long x) {cout << x;}
+void __print(unsigned x) {cout << x;}
+void __print(unsigned long x) {cout << x;}
+void __print(unsigned long long x) {cout << x;}
+void __print(float x) {cout << x;}
+void __print(double x) {cout << x;}
+void __print(long double x) {cout << x;}
+void __print(char x) {cout << '\'' << x << '\'';}
+void __print(const char *x) {cout << '"' << x << '"';}
+void __print(const string &x) {cout << '"' << x << '"';}
+void __print(bool x) {cout << (x ? "true" : "false");}
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cout << '{'; __print(x.first); cout << ','; __print(x.second); cout << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cout << '{'; for (auto &i: x) cout << (f++ ? "," : ""), __print(i); cout << "}";}
+void _print() {cout << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cout << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cout << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void helper(TreeNode* root,int maxval,int &ans){
+        if(root==NULL) return ;
+        if(root->val >= maxval){
+            ans++;
+            maxval = root->val;
+        }
+        helper(root->left,maxval,ans);
+        helper(root->right,maxval,ans);
+    }
+    int goodNodes(TreeNode* root) {
+        int ans = 0;
+        helper(root,INT_MIN,ans);
+        return ans;
+    }
+};
