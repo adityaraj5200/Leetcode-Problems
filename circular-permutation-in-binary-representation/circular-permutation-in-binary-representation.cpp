134f7044984f2845ae8f23c6e56b51ae7bb3d81d
Aditya Raj <72252036+adityaraj5200@users.noreply.github.com>
2021-11-17 17:49:04 +0530

    Time: 12 ms (93.53%), Space: 16.4 MB (94.53%) - LeetHub

--- circular-permutation-in-binary-representation/circular-permutation-in-binary-representation.cpp
+++ circular-permutation-in-binary-representation/circular-permutation-in-binary-representation.cpp
@@ -4,13 +4,9 @@ public:
         vector<int> result(1<<n);
         result[0] = 0;
         result[1] = 1;
-        int bitpos = 1, i = 1, j ;
-        while(--n){
-            j = i+1;
+        for(int i=1,j=i+1,bitpos=1;--n;i=j-1,bitpos++){
             while(i>=0)
                 result[j++] = result[i--] | (1<<bitpos);
-            bitpos++;
-            i = j-1;
         }
         const auto start_it = find(result.begin(), result.end(), start);
         rotate(result.begin(), start_it, result.end());
