ce96125f656c48dacdf8e36e60a8caccf3b9f867
Aditya Raj <72252036+adityaraj5200@users.noreply.github.com>
2022-03-05 19:14:03 +0530

    Time: 10 ms (61.81%), Space: 11.6 MB (32.19%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,21 +1,20 @@
-class Solution {
+class Solution{
 public:
-    int deleteAndEarn(vector<int>& nums) {
-        map<int,int> freq;
-        for(int &val: nums){
+    int deleteAndEarn(vector<int>& nums){
+        const int maxN = 1e4;
+        vector<int> freq(maxN+1,0);
+        for(int val: nums) 
             freq[val]++;
-        }
         
-        int inc = freq[1]; //include
-        int exc = 0; // exclude
-        int previnc = inc, prevexc = exc; // previous include, exclude;
+        int inc = freq[1];
+        int exc = 0;
+        int inc_prev=inc,exc_prev=exc;
         
-        for(int val=2;val<=1e4;val++){
-            inc = val*freq[val]+prevexc;
-            exc = max(previnc, prevexc);
-            
-            previnc = inc;
-            prevexc = exc;
+        for(int val=2;val<=maxN;val++){
+            inc = freq[val]*val + exc_prev;
+            exc = max(inc_prev,exc_prev);
+            inc_prev = inc;
+            exc_prev = exc;
         }
         
         return max(inc,exc);
