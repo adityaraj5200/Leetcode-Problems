e1d3caadd59c64a7a20e86a7da7a1b42945982ef
Aditya Raj <72252036+adityaraj5200@users.noreply.github.com>
2022-06-17 21:47:23 +0530

    Time: 4 ms (98.44%), Space: 21.1 MB (87.57%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.cpp
+++ 968-binary-tree-cameras/968-binary-tree-cameras.cpp
@@ -11,9 +11,10 @@
  */
 class Solution {
 public:
-    #define HAVE 0
-    #define COVERED 1
-    #define NEED 2
+    // Messages Nodes are sending to their parent node
+    #define HAVE 0 // The node is saying that I have camera so I am covering myself and I will also cover you
+    #define COVERED 1 // The node is saying I am covered, you arrange for yourself
+    #define NEED 2 // The node is saying I am not covered in anyway so install camera for me.
     
     int ans = 0;
     
@@ -24,20 +25,25 @@ public:
         int rightStatus = getStatus(root->right);
         
         if(leftStatus==NEED || rightStatus==NEED){
-            ans++;
+            // If any child said they need camera then install camera, and give message to the parent that you 'HAVE' the camera.
+            
+            ans++; // incrementing the camera count
             return HAVE;
         }
         
         if(leftStatus==HAVE || rightStatus==HAVE){
+            // If any of the child said that they have the camera then I will tell my parent that I do not have the camera but I am 'COVERED'
             return COVERED;
         }
         
+        // if control reached here that means both left and right child are saying they are covered. So I am not covered by anyone so I 'NEED' my parent to install camera.
         return NEED;
         
     }
     
     int minCameraCover(TreeNode* root) {
-        if(getStatus(root) == NEED) ans++;
+        if(getStatus(root) == NEED) ans++; // When the root node itself is a leaf node then we need at least one camera.
+        
         return ans;
     }
 };
