55997a771157b60b746fe2d28034231a8f430e4d
Aditya Raj <72252036+adityaraj5200@users.noreply.github.com>
2022-07-02 20:54:01 +0530

    Time: 88 ms (76.56%), Space: 32.4 MB (40.04%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
     int maxArea(int h, int w, vector<int>& horizontalCuts, vector<int>& verticalCuts) {
-        horizontalCuts.push_back(0);
-        horizontalCuts.push_back(h);
-        
-        verticalCuts.push_back(0);
-        verticalCuts.push_back(w);
-        
         sort(horizontalCuts.begin(),horizontalCuts.end());
         sort(verticalCuts.begin(),verticalCuts.end());
         
-        int best_hor_cuts_gap = 0, best_ver_cuts_gap = 0;
-        for(int i=1;i<horizontalCuts.size();i++){
-            best_hor_cuts_gap = max(best_hor_cuts_gap, horizontalCuts[i]-horizontalCuts[i-1]);
-        }
-        for(int i=1;i<verticalCuts.size();i++){
-            best_ver_cuts_gap = max(best_ver_cuts_gap, verticalCuts[i]-verticalCuts[i-1]);
-        }
+        int max_hor_gap = max(horizontalCuts[0],h-horizontalCuts.back());
+        int max_ver_gap = max(verticalCuts[0],w-verticalCuts.back());
         
-        return ((long long)best_hor_cuts_gap * (long long)best_ver_cuts_gap)%((long long)(1e9+7));
+        for(int i=1;i<horizontalCuts.size();i++)
+            max_hor_gap = max(max_hor_gap, horizontalCuts[i]-horizontalCuts[i-1]);
+        for(int i=1;i<verticalCuts.size();i++)
+            max_ver_gap = max(max_ver_gap, verticalCuts[i]-verticalCuts[i-1]);
+        
+        return ((long long)max_hor_gap * (long long)max_ver_gap) % ((long long)(1e9+7));
     }
 };
