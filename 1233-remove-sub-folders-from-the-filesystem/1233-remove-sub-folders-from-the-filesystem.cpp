3b2ecdb9933e992ff143aac37b5d796d6bffeba0
Aditya Raj <72252036+adityaraj5200@users.noreply.github.com>
2022-07-10 15:58:58 +0530

    Time: 257 ms (75.00%), Space: 45.3 MB (79.06%) - LeetHub

--- 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.cpp
+++ 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.cpp
@@ -1,81 +1,17 @@
 class Solution {
 public:
-    struct Trie{
-        unordered_map<string,Trie*> child;
-        // bool isEnd;
-        Trie(){
-            // isEnd = false;
-        }
-    };
-    
-    Trie *root;
-    
-    void insert(string &dir){
-        Trie *curr = root;
-        int idx = 1, n = dir.length();
-        while(idx<n){
-            string str = "";
-            while(idx<n && dir[idx]!='/')
-                str += dir[idx++];
-            idx++;
-            
-            if(curr->child.find(str) == curr->child.end())
-                curr->child[str] = new Trie();
-            
-            curr = curr->child[str];
-        }
-        // curr->isEnd = true;
-    }
-    
-    void remove(string &dir){ 
-        Trie *curr = root;
-        int idx = 1, n = dir.length();
-        while(idx<n){
-            string str = "";
-            while(idx<n && dir[idx]!='/')
-                str += dir[idx++];
-            idx++;
-            
-            if(curr->child.find(str) != curr->child.end())
-                curr = curr->child[str];
-            else break;
-        }
-        curr->child = {};
-        // curr->isEnd = true;
-    }
-    
-    vector<string> ans;
-    string currstr = "";
-    
-    void traverse(Trie* node){
-        if(node->child.size() == 0){
-            if(!currstr.empty())
-                ans.push_back(currstr);
-            
-            return;
+    vector<string> removeSubfolders(vector<string>& folder) {
+        sort(folder.begin(), folder.end());
+        vector<string> res;
+        res.push_back(folder[0]);
+        
+        for (int i = 1; i < folder.size(); i++) {
+            string root = res[res.size() - 1] + "/";
+            string prefix = folder[i].substr(0, root.size());
+            if (prefix != root)
+                res.push_back(folder[i]);
         }
         
-        for(auto p: node->child){
-            string foldername = p.first;
-            Trie* nextnode = p.second;
-            
-            currstr += "/" + foldername;
-            
-            traverse(node->child[foldername]);
-            
-            int pop_these = foldername.size() + 1;
-            while(pop_these--)
-                currstr.pop_back();
-        }
-    }
-    
-    vector<string> removeSubfolders(vector<string>& folder){
-        root = new Trie();
-        
-        for(string &s: folder) insert(s);
-        for(string &s: folder) remove(s);
-        traverse(root);
-        
-        return ans;
+        return res;
     }
 };
