d81dcd7072de3feed062a65c86be08a76308e588
Aditya Raj <72252036+adityaraj5200@users.noreply.github.com>
2022-07-12 14:32:39 +0530

    Time: 248 ms (63.34%), Space: 10.6 MB (27.71%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.cpp
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -1,41 +1,48 @@
 class Solution {
 public:
-    int each_side_len;
-    
-    bool recurse(vector<int>& nums,int idx,int curr_side_len,int sides_found){
-        if(sides_found == 4) // we can also make it 3
-            return true;
+    bool makesquare(vector<int>& nums) {
+        int n = nums.size();
+
+        long sum = accumulate(nums.begin(), nums.end(), 0l);
+        if (sum % 4) return false;
         
-        if(curr_side_len >= each_side_len)
-            return curr_side_len==each_side_len && recurse(nums,0,0,sides_found+1);
+        long sideLen = sum / 4;
+        // need to solve the problem of partitioning nums into four equal subsets each having
+        // sum equal to sideLen
+        vector<int> usedMasks;
+        // validHalfSubsets[i] == true iff the subset represented by bitmask i
+        // has sum == 2*sideLen, AND the subset represented by i can be further partitioned into
+        // two equal subsets. See below for how it is used.
+        vector<bool> validHalfSubsets(1<<n, false);
         
-        if(idx==nums.size())
-            return false;
-        
-        if(nums[idx] == 0)
-            return recurse(nums,idx+1,curr_side_len,sides_found);
-        
-        if(nums[idx] != 0){
-            int val = nums[idx];
-            nums[idx] = 0;
-            bool pick = recurse(nums,idx+1,curr_side_len+val,sides_found);
-            if(pick) return true;
-            nums[idx] = val;
+        // E.g., if n = 5, (1 << 5 - 1) = 11111 represents the whole set
+        int all = (1 << n) - 1;
+        // go through all possible subsets each represented by a bitmask
+        for (int mask = 0; mask <= all; mask++) {
+            long subsetSum = 0;
+            // calculate the sum of this subset
+            for (int i = 0; i < 32; i++) {
+                if ((mask >> i) & 1)
+                    subsetSum += nums[i];
+            }
+            // if this subset has what we want
+            if (subsetSum == sideLen) {
+                for (int usedMask : usedMasks) {
+                // if this mask and usedMask are mutually exclusive
+                    if ((usedMask & mask) == 0) {
+                    // then they form a valid half subset whose sum is 2 * sideLen,
+                            // that can be further partitioned into two equal subsets (usedMask and mask)
+                        int validHalf = usedMask | mask;
+                        validHalfSubsets[validHalf] = true;
+                        // if in the past we concluded that the other half is also a valid
+                        // half subset, DONE!
+                        if (validHalfSubsets[all ^ validHalf])
+                            return true;
+                    }
+                }
+                usedMasks.push_back(mask);
+            }
         }
-        
-        bool not_pick = recurse(nums,idx+1,curr_side_len,sides_found);
-        return not_pick;
-    }
-    
-    bool makesquare(vector<int>& matchsticks){
-        int total_sum = accumulate(matchsticks.begin(),matchsticks.end(),0);
-        if(total_sum % 4 != 0)
-            return false;
-        
-        sort(matchsticks.begin(),matchsticks.end(),greater<int>());
-        
-        each_side_len = total_sum/4;
-        
-        return recurse(matchsticks,0,0,0);
+        return false;
     }
 };
