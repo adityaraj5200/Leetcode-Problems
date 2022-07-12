52594f681470f71c19c04974bc33cfa127a95312
Aditya Raj <72252036+adityaraj5200@users.noreply.github.com>
2022-07-12 14:37:34 +0530

    Time: 891 ms (19.07%), Space: 10.2 MB (30.06%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.cpp
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -1,48 +1,26 @@
-class Solution {
+class Solution{
 public:
-    bool makesquare(vector<int>& nums) {
-        int n = nums.size();
-
-        long sum = accumulate(nums.begin(), nums.end(), 0l);
-        if (sum % 4) return false;
-        
-        long sideLen = sum / 4;
-        // need to solve the problem of partitioning nums into four equal subsets each having
-        // sum equal to sideLen
-        vector<int> usedMasks;
-        // validHalfSubsets[i] == true iff the subset represented by bitmask i
-        // has sum == 2*sideLen, AND the subset represented by i can be further partitioned into
-        // two equal subsets. See below for how it is used.
-        vector<bool> validHalfSubsets(1<<n, false);
-        
-        // E.g., if n = 5, (1 << 5 - 1) = 11111 represents the whole set
-        int all = (1 << n) - 1;
-        // go through all possible subsets each represented by a bitmask
-        for (int mask = 0; mask <= all; mask++) {
-            long subsetSum = 0;
-            // calculate the sum of this subset
-            for (int i = 0; i < 32; i++) {
-                if ((mask >> i) & 1)
-                    subsetSum += nums[i];
-            }
-            // if this subset has what we want
-            if (subsetSum == sideLen) {
-                for (int usedMask : usedMasks) {
-                // if this mask and usedMask are mutually exclusive
-                    if ((usedMask & mask) == 0) {
-                    // then they form a valid half subset whose sum is 2 * sideLen,
-                            // that can be further partitioned into two equal subsets (usedMask and mask)
-                        int validHalf = usedMask | mask;
-                        validHalfSubsets[validHalf] = true;
-                        // if in the past we concluded that the other half is also a valid
-                        // half subset, DONE!
-                        if (validHalfSubsets[all ^ validHalf])
-                            return true;
-                    }
-                }
-                usedMasks.push_back(mask);
-            }
+    int n,reqsum;
+    bool helper(vector<int>& nums,int index,vector<int>&sides){
+        if(index==n)
+            return all_of(sides.begin(),sides.end(),[&](int x){return x==0;});
+        for(int k=0;k<4;k++){
+            if(sides[k]-nums[index] < 0) continue;
+            sides[k] -= nums[index];
+            if(helper(nums,index+1,sides)) return true;
+            sides[k] += nums[index];
         }
         return false;
+    }
+    
+    bool makesquare(vector<int>& matchsticks) {
+        int sum = accumulate(matchsticks.begin(),matchsticks.end(),0);
+        n = matchsticks.size();
+        reqsum = sum/4;
+        sort(matchsticks.begin(),matchsticks.end(),greater<int>());
+        // sort(matchsticks.begin(),matchsticks.end());
+        if(sum%4 || matchsticks.size()<4) return false;
+        vector<int> sides(4,sum/4);
+        return helper(matchsticks,0,sides);
     }
 };
