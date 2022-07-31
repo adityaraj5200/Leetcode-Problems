4e94cb6e40d22600d986e17509621d161cb9dc24
Aditya Raj <72252036+adityaraj5200@users.noreply.github.com>
2022-07-31 23:00:19 +0530

    Time: 80 ms (14.35%), Space: 71.4 MB (50.84%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -11,16 +11,57 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode preHead, *ptr = &preHead;
-        int carry=0;
-        while(l1 || l2 || carry){
-            int sum = (l1?l1->val:0)+(l2?l2->val:0)+carry;
-            carry = sum/10;
-            ptr->next = new ListNode(sum%10);
-            ptr = ptr->next;
-            l1 = l1 ? l1->next : l1;
-            l2 = l2 ? l2->next : l2;
+        if(l1==NULL&&l2==NULL)
+            return l1;
+        int c=((l1==NULL?0:l1->val)+(l2==NULL?0:l2->val))/10;
+        ListNode *head=new ListNode(((l1==NULL?0:l1->val)+(l2==NULL?0:l2->val))%10);
+        l1=l1==NULL?l1:l1->next;
+        l2=l2==NULL?l1:l2->next;
+        ListNode *c1=head;
+        int i=0;
+        while(true)
+        {
+            if(l1==NULL&&l2==NULL)
+            {
+                if(c==0)
+                {
+                    cout<<"in c==0 "<<i++<<endl;
+                    return head;
+                }
+                cout<<"in NULL "<<i++<<endl;
+                c1=ins(c,c1);
+                return head;
+                
+            }
+            if(l1==NULL)
+            {
+                cout<<"in l1==NULL "<<i++<<endl;
+                c1=ins((l2->val+c)%10,c1);
+                c=(l2->val+c)/10;
+                l2=l2->next;
+            }
+            else if(l2==NULL)
+            {
+                cout<<"in l2==NULL "<<i++<<endl;
+                c1=ins((l1->val+c)%10,c1);
+                c=(l1->val+c)/10;
+                l1=l1->next;
+            }
+            else
+            {
+                cout<<"in else "<<i++<<endl;
+                c1=ins((l1->val+l2->val+c)%10,c1);
+                c=(l1->val+l2->val+c)/10;
+                l1=l1->next;
+                l2=l2->next;
+            }
         }
-        return preHead.next;
+        return head;
+    }
+    ListNode* ins(int n,ListNode* c)
+    {
+        ListNode *t=new ListNode(n);
+        c->next=t;
+        return c->next;
     }
 };
