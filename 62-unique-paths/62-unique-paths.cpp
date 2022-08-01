0762a9ae1575db905307206448a48a6ff0164e77
Aditya Raj <72252036+adityaraj5200@users.noreply.github.com>
2022-08-01 12:38:30 +0530

    Time: 2 ms (50.13%), Space: 6.2 MB (59.42%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<int> dp(n,1),temp(n,1);
+        while(--m){
+            temp[0] = 1;
+            for(int i=1;i<n;i++)
+                temp[i] = temp[i-1]+dp[i];
+            dp = temp;
+        }
+        return dp.back();
+    }
+};
