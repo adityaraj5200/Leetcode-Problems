4a9c6b1f3ec4883a4ac7e02d04a038d7ba34ea34
Aditya Raj <72252036+adityaraj5200@users.noreply.github.com>
2022-08-05 15:55:53 +0530

    Time: 7 ms (87.65%), Space: 11.2 MB (57.78%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -14,11 +14,11 @@ public:
         recurse(idx,candidates,target-candidates[idx],curr,ans);
         curr.pop_back();
         recurse(idx+1,candidates,target,curr,ans);
-        // recurse(idx+1,candidates,target,curr,ans);
     }
     
     vector<vector<int>> combinationSum(vector<int>& candidates, int target){
-        sort(candidates.begin(),candidates.end(),greater<int>());
+        // sort(candidates.begin(),candidates.end(),greater<int>());
+        sort(candidates.begin(),candidates.end());
         vector<int> curr;
         vector<vector<int>> ans;
         recurse(0,candidates,target,curr,ans);
