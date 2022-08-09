b63defd712c17f6ed933a1614d91d1cf06efedea
Aditya Raj <72252036+adityaraj5200@users.noreply.github.com>
2022-08-09 16:45:18 +0530

    Time: 11 ms (81.84%), Space: 8.3 MB (81.10%) - LeetHub

--- 6-zigzag-conversion/6-zigzag-conversion.cpp
+++ 6-zigzag-conversion/6-zigzag-conversion.cpp
@@ -1,38 +1,9 @@
-void __print(int x) {cout << x;}
-void __print(long x) {cout << x;}
-void __print(long long x) {cout << x;}
-void __print(unsigned x) {cout << x;}
-void __print(unsigned long x) {cout << x;}
-void __print(unsigned long long x) {cout << x;}
-void __print(float x) {cout << x;}
-void __print(double x) {cout << x;}
-void __print(long double x) {cout << x;}
-void __print(char x) {cout << '\'' << x << '\'';}
-void __print(const char *x) {cout << '"' << x << '"';}
-void __print(const string &x) {cout << '"' << x << '"';}
-void __print(bool x) {cout << (x ? "true" : "false");}
-
-template<typename T, typename V>
-void __print(const pair<T, V> &x) {cout << '{'; __print(x.first); cout << ','; __print(x.second); cout << '}';}
-template<typename T>
-void __print(const T &x) {int f = 0; cout << '{'; for (auto &i: x) cout << (f++ ? "," : ""), __print(i); cout << "}";}
-void _print() {cout << "]\n";}
-template <typename T, typename... V>
-void _print(T t, V... v) {__print(t); if (sizeof...(v)) cout << ", "; _print(v...);}
-
-#ifndef ONLINE_JUDGE
-#define debug(x...) cout << "[" << #x << "] = ["; _print(x)
-#define nline cout<<endl
-#else
-#define debug(x...)
-#define nline
-#endif
-
 class Solution {
 public:
     string convert(string s, int numRows) {
         const int n = s.length();
         if(numRows==1) return s;
+        
         string ans;
         
         for(int curr_row = 0; curr_row<numRows ; curr_row++){
