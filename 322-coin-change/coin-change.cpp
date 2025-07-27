4e56295377800f778e05bb91f3c92546d61e7673
Aditya Raj <72252036+adityaraj5200@users.noreply.github.com>
2025-07-27 16:50:20 +0530

    Time: 17 ms (87.60%) | Memory: 17.8 MB (85.18%) - LeetSync

---
+++ 322-coin-change/coin-change.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        sort(coins.begin(),coins.end());
+        while(coins.size() && coins.back()>amount){
+            coins.pop_back();
+        }
+        int n = coins.size();
+        vector<int> dp(amount+1,1e9);
+        dp[0] = 0;
+        for(int i=0;i<=amount;i++){
+            if(dp[i] == 1e9) continue;
+            for(int& coin: coins){
+                if(i+coin > amount) break;
+                dp[i+coin] = min(dp[i+coin], 1+dp[i]);
+            }
+        }
+
+        return dp[amount]==1e9 ? -1 : dp[amount];
+    }
+};
