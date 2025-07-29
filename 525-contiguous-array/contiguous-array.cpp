d39b476356e58d2d2ca96a4ba5f845c9d91d05ea
Aditya Raj <72252036+adityaraj5200@users.noreply.github.com>
2025-07-29 14:11:32 +0530

    Time: 2 ms (99.31%) | Memory: 90.3 MB (11.85%) - LeetSync

---
+++ 525-contiguous-array/contiguous-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        const int n = nums.size(), offset = n;
+		vector<int> seenSumAt(n+1+offset, INT_MIN);
+		seenSumAt[offset] = -1; // This is like saying that we saw sum (0+offset) at index -1
+        int maxLen = 0, sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += (nums[i] == 0 ? -1 : 1);
+			if (seenSumAt[sum + offset] >= -1){
+                maxLen = max(maxLen, i - seenSumAt[sum + offset]);
+            }
+			else seenSumAt[sum + offset] = i;
+        }
+        return maxLen;
+    }
+};
