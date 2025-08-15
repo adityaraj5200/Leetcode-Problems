d5b224652a2302765ea21dca8f103368e20cfa80
Aditya Raj <72252036+adityaraj5200@users.noreply.github.com>
2025-08-15 11:56:30 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (60.08%) - LeetSync

--- 342-power-of-four/power-of-four.java
+++ 342-power-of-four/power-of-four.java
@@ -1,13 +1,20 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        if(n<=0) return false;
-        if(n==1) return true;
-        int limit = Math.min(n,Integer.MAX_VALUE/4+1);
-        int val = 1;
-        while(val<limit){
-            val *= 4;
-        }
+        return (n>0) && (((n)&(n-1))==0) && (n-1)%3==0;
+        // 1 - 1
+        //2 - 10
+        //4 - 100
+        //8 - 10000
 
-        return val==n;
+        // Number of 0s to the right side of set bit(1) is always even
+        // 00
+        // 0000
+        // 000000
+
+        // Convert those 0s into 1s and think of their value
+        // 11 - 3
+        // 1111 - 15
+        // 111111 - 63
+        // Always divisible by 3
     }
 }
