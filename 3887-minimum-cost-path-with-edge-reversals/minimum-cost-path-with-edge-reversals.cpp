3fe89c11249d459871745c30692a2b0e77ed7bbc
Aditya Raj <72252036+adityaraj5200@users.noreply.github.com>
2025-08-16 22:01:02 +0530

    Time: 217 ms (89.70%) | Memory: 289.8 MB (97.49%) - LeetSync

--- 3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.cpp
+++ 3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.cpp
@@ -1,34 +1,29 @@
 class Solution {
 public:
     int minCost(int n, vector<vector<int>>& edges) {
-        vector<vector<pair<int,int>>> adj(n);
-        for(auto edge: edges){
-            adj[edge[0]].push_back({edge[1],edge[2]});
-            adj[edge[1]].push_back({edge[0],2*edge[2]});
+        vector<vector<pair<int, int>>>adj(n);
+        for(auto& edge : edges){
+            int u = edge[0], v =edge[1], w = edge[2];
+            adj[u].push_back({v,w});
+            adj[v].push_back({u, 2*w});
         }
-        
-        vector<int> minDist(n,1e9);
-        minDist[0] = 0;
+        vector<int>dist(n, INT_MAX);
+        priority_queue<pair<int, int>,
+            vector<pair<int,int>>, greater<pair<int, int>>>pq;
+        dist[0] = 0;
+        pq.push({0, 0});
 
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-        pq.push({0,0}); // {costSoFar, node}
-
-        while(!pq.empty()) {
-            auto [d, u] = pq.top();
-            pq.pop();
-
-    
-            if(d > minDist[u]) continue;
-            if(u==n-1) return d; // Reached destination
-    
-            for(auto [v, wt] : adj[u]) {
-                if(minDist[u] + wt < minDist[v]) {
-                    minDist[v] = minDist[u] + wt;
-                    pq.push({minDist[v], v});
+        while(!pq.empty()){
+            auto[d, u] = pq.top(); pq.pop();
+                if(d>dist[u]) continue;
+            if(u == n-1)return d;
+            for(auto[v,w] : adj[u]){
+                if(dist[u] + w < dist[v]){
+                    dist[v] = dist[u]+w;
+                    pq.push({dist[v], v});
                 }
             }
         }
-
         return -1;
     }
 };
