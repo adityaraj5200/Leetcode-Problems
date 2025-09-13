569c3d3115384a206d96ff994accefe585f96764
Aditya Raj <72252036+adityaraj5200@users.noreply.github.com>
2025-09-13 14:52:20 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (97.50%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.cpp
+++ 386-lexicographical-numbers/lexicographical-numbers.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     vector<int> lexicalOrder(int n) {
-        vector<int> ans;
-        for(int i=1,curr=1;i<=n;i++){
-            ans.push_back(curr);
-            if(curr*10<=n){
+        vector<int> result;
+        result.reserve(n);
+        
+        int curr = 1;
+        for(int i = 0; i < n; i++) {
+            result.push_back(curr);
+            
+            // Try to go "deeper" by appending a '0': e.g., 1 → 10
+            if(curr * 10 <= n) {
                 curr *= 10;
-            }
-            else{
-                curr++;
-                while(curr%10==0){
+            } 
+            else {
+                // If can't go deeper, try to increment
+                // But if ending digit is 9, or curr == n, we need to backtrack
+                while(curr % 10 == 9 || curr + 1 > n) {
                     curr /= 10;
                 }
-                if(curr>n){
-                    curr = curr/10 + 1;
-                }
-                while(curr%10==0){
-                    curr /= 10;
-                }
+                curr += 1;
             }
         }
-
-        return ans;
+        
+        return result;
     }
 };
