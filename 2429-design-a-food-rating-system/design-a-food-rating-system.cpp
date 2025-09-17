7e61cab896fadffbc9f0a007795cc36c60c27220
Aditya Raj <72252036+adityaraj5200@users.noreply.github.com>
2025-09-17 13:39:46 +0530

    Time: 169 ms (29.92%) | Memory: 174.2 MB (21.05%) - LeetSync

--- 2429-design-a-food-rating-system/design-a-food-rating-system.cpp
+++ 2429-design-a-food-rating-system/design-a-food-rating-system.cpp
@@ -1,56 +1,28 @@
 class FoodRatings {
-private:
-    struct Compare {
-        bool operator()(pair<int,string>& a,pair<int,string>& b){
-            if(a.first == b.first){
-                return a.second > b.second;
-            }
-            return a.first < b.first;
-        }
-    };
+    using pis = pair<int, string>;
 
-    map<string, priority_queue<pair<int,string>, vector<pair<int,string>>, Compare>> cuisineToHeap;
-
-    map<string,int> foodToRating;
-    map<string,string> foodToCuisine;
+    unordered_map<string, string> foodCuisine;
+    unordered_map<string, int> foodRating;
+    unordered_map<string, set<pis>> mp;
 
 public:
-    FoodRatings(vector<string>& foods, vector<string>& cuisines, vector<int>& ratings) {
-        const int n = foods.size();
-        for(int i=0;i<n;i++){
-            cuisineToHeap[cuisines[i]].push({ratings[i],foods[i]});
-            foodToRating[foods[i]] = ratings[i];
-            foodToCuisine[foods[i]] = cuisines[i];
+    FoodRatings(vector<string>& foods, vector<string>& cuisines,
+                vector<int>& ratings) {
+        for (int i = 0; i < foods.size(); i++) {
+            foodCuisine[foods[i]] = cuisines[i];
+            foodRating[foods[i]] = ratings[i];
+            mp[cuisines[i]].insert({-ratings[i], foods[i]});
         }
     }
-    
+
     void changeRating(string food, int newRating) {
-        foodToRating[food] = newRating;
-        string cuisine = foodToCuisine[food];
-        cuisineToHeap[cuisine].push({newRating,food});
-    }
-    
-    string highestRated(string cuisine) {
-        string highestRatedFood;
-        while(true){
-            auto [rating, food] = cuisineToHeap[cuisine].top();
-            
-            if(rating==foodToRating[food]){
-                highestRatedFood = food;
-                break;
-            }
-            else{
-                cuisineToHeap[cuisine].pop();
-            }
-        }
+        string cuisine = foodCuisine[food];
+        int oldRating = foodRating[food];
 
-        return highestRatedFood;
+        mp[cuisine].erase({-oldRating, food});
+        mp[cuisine].insert({-newRating, food});
+        foodRating[food] = newRating;
     }
+
+    string highestRated(string cuisine) { return mp[cuisine].begin()->second; }
 };
-
-/**
- * Your FoodRatings object will be instantiated and called as such:
- * FoodRatings* obj = new FoodRatings(foods, cuisines, ratings);
- * obj->changeRating(food,newRating);
- * string param_2 = obj->highestRated(cuisine);
- */
