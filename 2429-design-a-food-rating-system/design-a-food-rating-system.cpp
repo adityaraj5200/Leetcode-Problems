44fc88f734f236ec56ee787168f98e3b70df51b2
Aditya Raj <72252036+adityaraj5200@users.noreply.github.com>
2025-09-17 13:41:04 +0530

    Time: 91 ms (95.84%) | Memory: 159.8 MB (95.57%) - LeetSync

--- 2429-design-a-food-rating-system/design-a-food-rating-system.cpp
+++ 2429-design-a-food-rating-system/design-a-food-rating-system.cpp
@@ -1,28 +1,41 @@
 class FoodRatings {
-    using pis = pair<int, string>;
-
-    unordered_map<string, string> foodCuisine;
-    unordered_map<string, int> foodRating;
-    unordered_map<string, set<pis>> mp;
-
 public:
-    FoodRatings(vector<string>& foods, vector<string>& cuisines,
-                vector<int>& ratings) {
-        for (int i = 0; i < foods.size(); i++) {
-            foodCuisine[foods[i]] = cuisines[i];
-            foodRating[foods[i]] = ratings[i];
-            mp[cuisines[i]].insert({-ratings[i], foods[i]});
+    struct Cmp {
+        bool operator()(const auto& a, const auto& b) const {
+            if(a.first != b.first) {
+                return a < b;
+            }
+            return a.second > b.second;
+        }
+    };
+
+    unordered_map<string, priority_queue<pair<int, string>, vector<pair<int, string>>, Cmp>> c2f;
+    unordered_map<string, pair<string, int>> f2c;
+
+    FoodRatings(vector<string>& foods, vector<string>& cuisines, vector<int>& ratings) {
+        int n = foods.size();
+        for(int i = 0; i < n; ++i) {
+            f2c[foods[i]] = {cuisines[i], ratings[i]};
+            c2f[cuisines[i]].emplace(ratings[i], foods[i]);
         }
     }
-
+    
     void changeRating(string food, int newRating) {
-        string cuisine = foodCuisine[food];
-        int oldRating = foodRating[food];
-
-        mp[cuisine].erase({-oldRating, food});
-        mp[cuisine].insert({-newRating, food});
-        foodRating[food] = newRating;
+        f2c[food].second = newRating;
+        c2f[f2c[food].first].emplace(newRating, food);
     }
-
-    string highestRated(string cuisine) { return mp[cuisine].begin()->second; }
+    
+    string highestRated(string cuisine) {
+        while(c2f[cuisine].top().first != f2c[c2f[cuisine].top().second].second) {
+            c2f[cuisine].pop();
+        }
+        return c2f[cuisine].top().second;
+    }
 };
+
+/**
+ * Your FoodRatings object will be instantiated and called as such:
+ * FoodRatings* obj = new FoodRatings(foods, cuisines, ratings);
+ * obj->changeRating(food,newRating);
+ * string param_2 = obj->highestRated(cuisine);
+ */
