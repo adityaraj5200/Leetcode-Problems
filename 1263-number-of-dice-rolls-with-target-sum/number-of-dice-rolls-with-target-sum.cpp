f0699ccd5c10145d315aea3106a7a6439b9ca83c
Aditya Raj <72252036+adityaraj5200@users.noreply.github.com>
2025-10-17 01:24:51 +0530

    Time: 11 ms (78.71%) | Memory: 11.3 MB (37.80%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
@@ -1,25 +1,24 @@
-// Time Complexity: O(n * k * target)
-// Space Complexity: O(n * target) + O(n) recursion stack
-
 class Solution {
-public:
-    const int MOD=1e9+7;
+private:
     vector<vector<int>> dp;
+    const int MOD = 1e9+7;
 
-    int solve(int dice,int faces,int target){
+    int solve(int n,int k,int target){
+        if(n==0) return target==0 ? 1 : 0;
         if(target<0) return 0;
-        if(dice==0) return target==0 ? 1 : 0;
 
-        if(dp[dice][target]!=-1) return dp[dice][target];
+        if(dp[n][target] != -1) return dp[n][target];
 
-        long long ways=0;
-        for(int i=1;i<=faces;i++){
-            ways=(ways+solve(dice-1,faces,target-i))%MOD;
+        long long numWays = 0;
+        for(int i=1;i<=k;i++){
+            numWays = (numWays+solve(n-1,k,target-i))%MOD;
         }
-        return dp[dice][target]=ways;
+
+        return dp[n][target] = numWays;
     }
 
-    int numRollsToTarget(int n,int k,int target){
+public:
+    int numRollsToTarget(int n, int k, int target) {
         dp.assign(n+1,vector<int>(target+1,-1));
         return solve(n,k,target);
     }
