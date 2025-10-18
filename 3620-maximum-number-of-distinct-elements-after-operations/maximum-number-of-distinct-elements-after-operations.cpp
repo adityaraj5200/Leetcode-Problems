a78ad87903d45789973cb59e3c49e9af2542bbc0
Aditya Raj <72252036+adityaraj5200@users.noreply.github.com>
2025-10-18 12:36:47 +0530

    Time: 240 ms (21.77%) | Memory: 147.9 MB (22.71%) - LeetSync

--- 3620-maximum-number-of-distinct-elements-after-operations/maximum-number-of-distinct-elements-after-operations.cpp
+++ 3620-maximum-number-of-distinct-elements-after-operations/maximum-number-of-distinct-elements-after-operations.cpp
@@ -3,7 +3,7 @@ public:
     int maxDistinctElements(vector<int>& nums, int k) {
         sort(nums.begin(),nums.end());
         int lastVal = INT_MIN;
-        set<int> st;
+        unordered_set<int> st;
         for(int& num: nums){
             int newNum = max(num-k, lastVal+1); // Lower bound is (num-k)
             newNum = min(newNum, num+k); // Upper bound is (num+k)
