e6562175b2e9658a1b360f064ffd35a58e6b6273
Aditya Raj <72252036+adityaraj5200@users.noreply.github.com>
2025-10-22 15:48:23 +0530

    Time: 183 ms (92.94%) | Memory: 10.5 MB (84.90%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,13 +1,13 @@
-// Time Complexity: O(m * n * 4^L)
+// Time Complexity: O(r * c * 4^L)
 // Space Complexity: O(L) — recursion stack depth (L = length of the word)
 
 class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        int m=board.size();
-        int n=board[0].size();
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        r=board.size();
+        c=board[0].size();
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
                 if(board[i][j]==word[0] && dfs(board,word,i,j,0)){
                     return true;
                 }
@@ -17,10 +17,10 @@ public:
     }
 
 private:
+    int r,c;
     bool dfs(vector<vector<char>>& board,const string& word,int i,int j,int k){
         if(k==word.size()) return true;
-        int m=board.size(),n=board[0].size();
-        if(i<0||j<0||i>=m||j>=n||board[i][j]!=word[k]) return false;
+        if(i<0||j<0||i>=r||j>=c||board[i][j]!=word[k]) return false;
 
         char temp=board[i][j];
         board[i][j]='#'; // mark visited
