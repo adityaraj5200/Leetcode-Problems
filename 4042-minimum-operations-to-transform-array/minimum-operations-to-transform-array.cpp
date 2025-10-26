b44e431a5cf22a5a016f75ee8095e9bb38af85e1
Aditya Raj <72252036+adityaraj5200@users.noreply.github.com>
2025-10-26 14:46:05 +0530

    Time: 0 ms (100.00%) | Memory: 213.9 MB (0.00%) - LeetSync

---
+++ 4042-minimum-operations-to-transform-array/minimum-operations-to-transform-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    long long minOperations(vector<int>& A, vector<int>& B) {
+        int v = B.back(), last = 1e9;
+        long long res = 0;
+        for (int i = 0; i < A.size(); ++i) {
+            int a = A[i], b = B[i];
+            res += abs(a - b);
+            // check if 'v' is between a and b.
+            if ((a <= v && v <= b) || (b <= v && v <= a))
+                last = 0;
+            else {
+                // if not then what is the min distance between v and either ends
+                last = min(last, min(abs(a - v), abs(b - v)));
+            }
+        }
+        return res + last + 1;
+    }
+};
