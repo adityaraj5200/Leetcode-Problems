6b4c56ddd94fc1318de0e6b394242039973b5db2
Aditya Raj <72252036+adityaraj5200@users.noreply.github.com>
2025-10-27 14:08:02 +0530

    Time: 8 ms (48.88%) | Memory: 27.2 MB (95.94%) - LeetSync

--- 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.cpp
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.cpp
@@ -1,20 +1,23 @@
+// Time Complexity: O(m * n) where m = number of rows, n = number of columns
+// Space Complexity: O(1)
+
+// Approach:
+// • For each row, count the number of '1's (security devices).
+// • Whenever we find a non-empty row, multiply its count with the previous non-empty row's count.
+// • Keep updating the last non-empty row’s device count.
+
 class Solution {
 public:
     int numberOfBeams(vector<string>& bank) {
-        int lastNumSecurityDevies = 0, ans = 0;
-        for(string& s: bank){
-            int numSecurityDeviesInCurrentRow = 0;
-            for(char& ch: s){
-                if(ch=='1'){
-                    numSecurityDeviesInCurrentRow++;
-                }
-            }
-            if(numSecurityDeviesInCurrentRow > 0){
-                ans += numSecurityDeviesInCurrentRow*lastNumSecurityDevies;
-                lastNumSecurityDevies = numSecurityDeviesInCurrentRow;
+        int prevDevices = 0, totalBeams = 0;
+        
+        for (const string& row : bank) {
+            int currDevices = count(row.begin(), row.end(), '1');
+            if (currDevices > 0) {
+                totalBeams += prevDevices * currDevices;
+                prevDevices = currDevices;
             }
         }
-
-        return ans;
+        return totalBeams;
     }
 };
