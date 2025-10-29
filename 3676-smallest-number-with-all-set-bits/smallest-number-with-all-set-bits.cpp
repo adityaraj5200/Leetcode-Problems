7433ea07832d30419f084a12b88f0c6d95160a42
Aditya Raj <72252036+adityaraj5200@users.noreply.github.com>
2025-10-29 17:18:00 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (60.80%) - LeetSync

--- 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.cpp
+++ 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.cpp
@@ -1,11 +1,18 @@
+// Time Complexity: O(log n)
+// Space Complexity: O(1)
+
+// Approach:
+// • Recognize that numbers with binary representation of all 1’s are of the form 2^k−1 (e.g., 1, 3, 7, 15, ...).
+// • Find the smallest power-of-two x such that x−1 ≥ n.
+// • Return x−1, which has all bits set and is the smallest such number ≥ n.
+
 class Solution {
 public:
     int smallestNumber(int n) {
         int x = 1;
-        while(x-1 < n){
-            x <<= 1;
+        while (x - 1 < n) {
+            x <<= 1; // move to next power of two
         }
-
-        return x-1;
+        return x - 1;
     }
 };
