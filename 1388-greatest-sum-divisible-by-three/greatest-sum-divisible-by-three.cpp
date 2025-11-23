c14549071c0f3ed49974c36cba7e36f1e5e3034c
Aditya Raj <72252036+adityaraj5200@users.noreply.github.com>
2025-11-23 15:35:23 +0530

    Time: 0 ms (100.00%) | Memory: 28.9 MB (84.90%) - LeetSync

--- 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
+++ 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
@@ -1,36 +1,47 @@
+// Approach:
+// - We want the maximum sum of a subset of nums such that sum % 3 == 0.
+// - Observe that any number x has remainder r = x % 3 (r in {0,1,2}).
+// - We maintain dp[0], dp[1], dp[2]: the maximum sum we can achieve so far with remainder 0,1,2 respectively.
+// - For each num in nums:
+//     we consider adding it to each of the three previous states and update a new dp array:
+//       newDp[(rPrev + num % 3) % 3] = max(newDp[(rPrev + num % 3) % 3], dp[rPrev] + num)
+//     Also we can always skip num: newDp[rPrev] = max(newDp[rPrev], dp[rPrev])
+// - At the end dp[0] is the answer (largest sum divisible by 3).
+//
+// Time Complexity: O(n) where n = nums.size()
+// Space Complexity: O(1) extra space (just 3 variables for dp states)
+
 class Solution {
 public:
     int maxSumDivThree(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        vector<int> remainder[3];
-        int totalSum = 0;
-        for(int &num: nums){
-            remainder[num%3].push_back(num);
-            totalSum += num;
+        // dp[0] = max sum with remainder 0
+        // dp[1] = max sum with remainder 1
+        // dp[2] = max sum with remainder 2
+        int dp0 = 0, dp1 = INT_MIN, dp2 = INT_MIN;
+        
+        for (int x : nums) {
+            int r = x % 3;
+            int new0 = dp0, new1 = dp1, new2 = dp2;
+            
+            if (r == 0) {
+                new0 = max(new0, dp0 + x);
+                new1 = max(new1, dp1 + x);
+                new2 = max(new2, dp2 + x);
+            } else if (r == 1) {
+                new1 = max(new1, dp0 + x);
+                new2 = max(new2, dp1 + x);
+                new0 = max(new0, dp2 + x);
+            } else { // r == 2
+                new2 = max(new2, dp0 + x);
+                new0 = max(new0, dp1 + x);
+                new1 = max(new1, dp2 + x);
+            }
+            
+            dp0 = new0;
+            dp1 = new1;
+            dp2 = new2;
         }
         
-        int rem = totalSum%3;
-        int ans = INT_MIN;
-        if(rem==0){
-            ans = totalSum;
-        }
-        else if(rem==1){
-            if(remainder[1].size()>=1){
-                ans = max(ans, totalSum-remainder[1][0]);
-            }
-            if(remainder[2].size()>=2){
-                ans = max(ans, totalSum-(remainder[2][0]+remainder[2][1]));
-            }
-        }
-        else if(rem==2){
-            if(remainder[2].size()>=1){
-                ans = max(ans, totalSum-remainder[2][0]);
-            }
-            if(remainder[1].size()>=2){
-                ans = max(ans, totalSum-(remainder[1][0]+remainder[1][1]));
-            }
-        }
-
-        return ans;
+        return dp0;
     }
 };
