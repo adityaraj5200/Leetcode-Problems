c364cdd9b4b29d554cae69cd902fe3b87803d385
Aditya Raj <72252036+adityaraj5200@users.noreply.github.com>
2025-12-10 10:23:42 +0530

    Time: 2 ms (74.67%) | Memory: 92.2 MB (5.33%) - LeetSync

--- 3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.java
+++ 3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.java
@@ -1,13 +1,11 @@
 class Solution {
     public int countPermutations(int[] complexity) {
+        long ans = 1, factorial = complexity.length-1, MOD = (long)1e9+7L;
         for(int i=1;i<complexity.length;i++){
+            ans = (ans*i)%MOD;
+            
             if(complexity[i] <= complexity[0])
                 return 0;
-        }
-
-        long ans = 1, factorial = complexity.length-1, MOD = (long)1e9+7L;
-        for(long f=factorial;f>0;f--){
-            ans = (ans*f)%MOD;
         }
 
         return (int)ans;
